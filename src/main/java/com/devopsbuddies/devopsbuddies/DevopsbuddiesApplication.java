09b78eab03c6986020615545683e2975d4bf7a5b
witnessManyange <33064724+witnessManyange@users.noreply.github.com>
2017-10-25 17:33:50 +0200

    Initial helloworld program

---
+++ src/main/java/com/devopsbuddies/devopsbuddies/DevopsbuddiesApplication.java
@@ -0,0 +1,12 @@
+package com.devopsbuddies.devopsbuddies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsbuddiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsbuddiesApplication.class, args);
+	}
+}
